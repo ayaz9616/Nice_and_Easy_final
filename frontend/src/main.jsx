a8d77722074beeddeb6c6b25aa4672bbdd761414
ayaz9616 <157129181+ayaz9616@users.noreply.github.com>
2025-03-12 12:33:55 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,9 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-<<<<<<< HEAD
-=======
+
 // export const server = "http://localhost:5000";
->>>>>>> a888b3ba (Update backend URL in .env file)
+
 export const server = "https://nice-and-easy-final.vercel.app/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
